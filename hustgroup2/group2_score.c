95f3372de1cb0184ccb00b7e3d476db33ee07144
LuyaoSun <jacob_226@live.com>
2014-06-07 14:14:35 +0800

    name

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -6,7 +6,7 @@ printf("This is for HUST ESD course 2014 score\n");
 printf("buyao,byforbest, 1,90\n");
 printf("CaoRong,freewind2014, 1,90\n");
 printf("TanDongcai,tancongcai, 1,90\n");
-printf("sunluyao,LuyaoSun, 1,90\n");
+printf("SunLuyao,LuyaoSun, 1,90\n");
 printf("yeyuanqing,yuanqingyeah, 1,90\n");
 return 0;
 
