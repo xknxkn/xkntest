51bf84300ad38a07772a26208ffadd555405ef00
slowsharp <837783277@qq.com>
2014-06-07 13:32:43 +0800

    Update group1_score.c

--- hustgroup6/group1_score.c
+++ hustgroup6/group1_score.c
@@ -4,7 +4,7 @@ main()
 {
 
 //printf("This is for HUST ESD course 2014 score\n"); 
-printf("Xufengli, slowsharp, 5, 90\n");
+printf("Xufengli, slowsharp, 6, 90\n");
 printf("Liugaoyang, hust65536, 6, 90\n");
 printf("Luzenong, KardLu, 5, 90\n");
 printf("Linmiaomiao, KardLu, 5, 90\n");
