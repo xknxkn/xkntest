8ab2085d51acfbd252b9960f39402b7decc967aa
熊峰 <1820342304@qq.com>
2014-06-07 09:32:18 +0800

    second

--- hustgroup5/group1_score.c
+++ hustgroup5/group1_score.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-main()
+int main()
 {
-
 printf("This is for HUST ESD course 2014 score\n"); 
 printf("qiaoyang,yang1young, 1,100\n");
-
+printf("xiongfeng,xiongbanban,1,99\n");
+printf("chenchen,KKNWtinacc,1,100\n");
+printf("chengtianchang,CTCbaxia,1,100\n");
+printf("cuihaoran,cuihaoran,1,100\n");
+return 0;
 }
