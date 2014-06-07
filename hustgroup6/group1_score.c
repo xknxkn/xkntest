17301b3cc5d9eef95194141ee29ece610610c2df
hust65536 <452215095@qq.com>
2014-06-07 12:02:38 +0800

    Update group1_score.c

--- hustgroup6/group1_score.c
+++ hustgroup6/group1_score.c
@@ -5,7 +5,7 @@ main()
 
 //printf("This is for HUST ESD course 2014 score\n"); 
 printf("Xuhao, shmilyhh, 5, 90\n");
-printf("Xinyu, FCSCanal, 5, 90\n");
+printf("Liugaoyang, hust65536, 6, 90\n");
 printf("Luzenong, KardLu, 5, 90\n");
 printf("Linmiaomiao, KardLu, 5, 90\n");
 printf("houyuhan,hyieeh,5,90\n");
