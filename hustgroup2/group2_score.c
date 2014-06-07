316fcfccdcc12e3ea71f85892d47c1c95cd69cf4
byforbest <343775598@qq.com>
2014-06-07 11:00:07 +0800

    name

---
+++ hustgroup2/group2_score.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+printf("This is for HUST ESD course 2014 score\n"); 
+printf("buyao,byforbest, 1,90\n");
+printf("caorong,freewind2014, 1,90\n");
+printf("tandongcai,tancongcai, 1,90\n");
+printf("sunluyao,LuyaoSun, 1,90\n");
+printf("yeyuanqing,yuanqingyeah, 1,90\n");
+return 0;
+
+}
