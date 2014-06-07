66749eecadbc6b1ddd8d66b509ce543fc9988e6b
xiaoxuan <2274765523@qq.com>
2014-06-07 13:48:22 +0800

    xx

--- hustgroup1/group1_score.c
+++ hustgroup1/group1_score.c
@@ -9,11 +9,7 @@ printf("Xinyu, FCSCanal, 5, 90\n");
 printf("Luzenong, KardLu, 5, 90\n");
 printf("Linmiaomiao, linmiaomiao, 5, 90\n");
 printf("houyuhan,hyieeh,5,90\n");
-<<<<<<< HEAD
 printf("wangyuhao,wyhgithub,5,90\n");
-=======
-printf("test");
->>>>>>> 7e7246890a3d3ab3dc018999b144f679dbd87100
 return 0;
 
 }
