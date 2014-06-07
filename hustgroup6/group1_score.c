920d4a06c486170964dcbba67f0564a5d7acb867
hgz <1023481339@qq.com>
2014-06-07 13:34:11 +0800

    Update group1_score.c

--- hustgroup6/group1_score.c
+++ hustgroup6/group1_score.c
@@ -6,7 +6,7 @@ main()
 //printf("This is for HUST ESD course 2014 score\n"); 
 printf("Xufengli, slowsharp, 6, 90\n");
 printf("Liugaoyang, hust65536, 6, 90\n");
-printf("Luzenong, KardLu, 5, 90\n");
+printf("HuangGuangZhou, husthgz, 6, 90\n");
 printf("Linmiaomiao, KardLu, 5, 90\n");
 return 0;
 
