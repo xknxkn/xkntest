da324b561838947c1b4bca007cc4cc2747948b40
freewind2014 <2278873915@qq.com>
2014-06-07 14:11:59 +0800

    TDC

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -5,7 +5,7 @@ main()
 printf("This is for HUST ESD course 2014 score\n"); 
 printf("buyao,byforbest, 1,90\n");
 printf("CaoRong,freewind2014, 1,90\n");
-printf("tandongcai,tancongcai, 1,90\n");
+printf("TanDongcai,tancongcai, 1,90\n");
 printf("sunluyao,LuyaoSun, 1,90\n");
 printf("yeyuanqing,yuanqingyeah, 1,90\n");
 return 0;
