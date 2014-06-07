09b4d4307b121a1e691ae7ac7160b56e3d7bdd61
熊峰 <1820342304@qq.com>
2014-06-07 11:32:57 +0800

    final

---
+++ hustgroup5/group5_score.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+printf("qiaoyang,yang1young, 1,100\n");
+printf("xiongfeng,xiongbanban,1,99\n");
+printf("chenchen,KKNWtinacc,1,100\n");
+printf("chengtianchang,CTCbaxia,1,100\n");
+printf("cuihaoran,cuihaoran,1,100\n");
+return 0;
+}
