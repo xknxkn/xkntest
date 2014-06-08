e1944285c86a86eb51adf28532dfe79d5ca04043
熊峰 <1820342304@qq.com>
2014-06-08 11:33:02 +0800

    test

--- hustgroup5/group5_score.c
+++ hustgroup5/group5_score.c
@@ -2,8 +2,10 @@
 
 int main()
 {
-printf("qiaoyang,yang1young, 1,100\n");
 printf("xiongfeng,xiongbanban,1,99\n");
 printf("xiongfeng,xiongbanban,1,80\n");
+printf("xiongfeng,xiongbanban,1,70\n");
+
+
 return 0;
 }
