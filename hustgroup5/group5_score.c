a760f1fbcd9e01dd94f33ca31d69d4c977f904fb
熊峰 <1820342304@qq.com>
2014-06-08 11:52:09 +0800

    x and cui

--- hustgroup5/group5_score.c
+++ hustgroup5/group5_score.c
@@ -2,10 +2,19 @@
 
 int main()
 {
-printf("xiongfeng,xiongbanban,1,99\n");
+printf("xiongfeng,xiongbanban,1,80\n");
+printf("xiongfeng,xiongbanban,1,80\n");
 printf("xiongfeng,xiongbanban,1,80\n");
 printf("xiongfeng,xiongbanban,1,70\n");
+printf("xiongfeng,xiongbanban,1,85\n");
 
+printf("cuihaoran,cuihaoran,1,80\n");
 
+printf("cuihaoran,cuihaoran,1,80\n");
+
+printf("cuihaoran,cuihaoran,1,80\n");
+printf("cuihaoran,cuihaoran,1,70\n");
+
+printf("cuihaoran,cuihaoran,1,85\n");
 return 0;
 }
