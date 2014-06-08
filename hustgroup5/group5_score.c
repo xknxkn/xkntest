fa060dda316db261d8e9e51f9ef77001202b16a9
熊峰 <1820342304@qq.com>
2014-06-08 12:02:53 +0800

    3

--- hustgroup5/group5_score.c
+++ hustgroup5/group5_score.c
@@ -3,18 +3,26 @@
 int main()
 {
 printf("xiongfeng,xiongbanban,1,80\n");
-printf("xiongfeng,xiongbanban,1,80\n");
-printf("xiongfeng,xiongbanban,1,80\n");
-printf("xiongfeng,xiongbanban,1,70\n");
-printf("xiongfeng,xiongbanban,1,85\n");
+printf("xiongfeng,xiongbanban,2,80\n");
+printf("xiongfeng,xiongbanban,3,80\n");
+printf("xiongfeng,xiongbanban,4,70\n");
+printf("xiongfeng,xiongbanban,5,85\n");
 
 printf("cuihaoran,cuihaoran,1,80\n");
+printf("cuihaoran,cuihaoran,2,80\n");
+printf("cuihaoran,cuihaoran,3,80\n");
+printf("cuihaoran,cuihaoran,4,70\n");
+printf("cuihaoran,cuihaoran,5,85\n");
 
-printf("cuihaoran,cuihaoran,1,80\n");
+printf("chengtianchang,CTCbaxia,1,85\n");
+printf("chengtianchang,CTCbaxia,2,85\n");
+printf("chengtianchang,CTCbaxia,3,80\n");
+printf("chengtianchang,CTCbaxia,4,85\n");
+printf("chengtianchang,CTCbaxia,5,85\n");
 
-printf("cuihaoran,cuihaoran,1,80\n");
-printf("cuihaoran,cuihaoran,1,70\n");
 
-printf("cuihaoran,cuihaoran,1,85\n");
+
+
 return 0;
+
 }
