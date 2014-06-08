ed417ba9e871b65e40fc3a03bca760d1854fc1e7
“lingjiajia” <1814739092@qq.com>
2014-06-08 17:40:54 +0800

    group4_lll

--- hustgroup4/group1_score.c
+++ hustgroup4/group1_score.c
@@ -2,11 +2,11 @@
 
 main()
 {
-printf("吕鹏原,lvpyuan, 4,90\n");
-printf("薛彧,dowhat, 4,90\n");
+printf("吕鹏原,lvpyuan, 4,91\n");
+printf("薛彧,dowhat, 4,91\n");
 printf("杜诗灏,LoveCD, 4,99\n");
-printf("肖璇,kittyxiao, 4,90\n");
-printf("凌佳佳,lingjiajia, 4,90\n");
+printf("肖璇,kittyxiao, 4,91\n");
+printf("凌佳佳,lingjiajia, 4,91\n");
 return 0;
 
 }
