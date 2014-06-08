f10676d6d22514a49059bb8eef80c1f290f334d7
熊峰 <1820342304@qq.com>
2014-06-08 11:25:23 +0800

    x

--- hustgroup5/group5_score.c
+++ hustgroup5/group5_score.c
@@ -4,8 +4,6 @@ int main()
 {
 printf("qiaoyang,yang1young, 1,100\n");
 printf("xiongfeng,xiongbanban,1,99\n");
-printf("chenchen,KKNWtinacc,1,100\n");
-printf("chengtianchang,CTCbaxia,1,100\n");
-printf("cuihaoran,cuihaoran,1,100\n");
+printf("xiongfeng,xiongbanban,1,80\n");
 return 0;
 }
