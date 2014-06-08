4300ed2592e58bf0d95b755001074f5c53410ee7
yuanqingyeah <496960691@qq.com>
2014-06-08 09:07:20 +0800

    name

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -7,7 +7,7 @@ printf("buyao,byforbest, 1,90\n");
 printf("CaoRong,freewind2014, 1,90\n");
 printf("TanDongcai,tancongcai, 1,90\n");
 printf("SunLuyao,LuyaoSun, 1,90\n");
-printf("yeyuanqing,yuanqingyeah, 1,90\n");
+printf("Yeyuanqing,yuanqingyeah, 1,90\n");
 return 0;
 
 }
