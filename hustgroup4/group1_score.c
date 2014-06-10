ec3fa03314ac89e3bc5cb0339a68592af48c9543
lvpyuan <1293353730@qq.com>
2014-06-10 16:27:54 +0800

    lvpyuan modify

--- hustgroup4/group1_score.c
+++ hustgroup4/group1_score.c
@@ -3,9 +3,22 @@
 main()
 {
 //printf("Group,Name,GithubID,Chapter,Section,Score\n");
-printf("吕鹏原,lvpyuan, 4,91\n");
-printf("薛彧,dowhat, 4,93\n");
-printf("杜诗灏,LoveCD, 4,99\n");
+
+printf("group4,吕鹏原,lvpyuan,Lab1-1,11,80\n");
+printf("group4,吕鹏原,lvpyuan,Lab1-2,11,80\n");
+printf("group4,吕鹏原,lvpyuan,Lab2-1,1,100\n");
+printf("group4,吕鹏原,lvpyuan,Lab2-2,2,90\n");
+printf("group4,吕鹏原,lvpyuan,Lab2-3,1,100\n");
+printf("group4,吕鹏原,lvpyuan,Lab2-4,1,100\n");
+printf("group4,吕鹏原,lvpyuan,Lab2-5,2,90\n");
+printf("group4,吕鹏原,lvpyuan,Lab3-1,3,80\n");
+printf("group4,吕鹏原,lvpyuan,Lab3-2,3,80\n");
+printf("group4,吕鹏原,lvpyuan,Lab3-3,5,70\n");
+printf("group4,吕鹏原,lvpyuan,Lab3-4,4,75\n");
+printf("group4,吕鹏原,lvpyuan,Lab4-1,1,100\n");
+printf("group4,吕鹏原,lvpyuan,Lab4-2,1,100\n");
+printf("group4,吕鹏原,lvpyuan,Lab4-3,2,98\n");
+printf("group4,吕鹏原,lvpyuan,Lab4-4,1,100\n");
 printf("group4,肖璇,kittyxiao,Lab1-1,10,80\n");
 printf("group4,肖璇,kittyxiao,Lab1-2,15,70\n");
 printf("group4,肖璇,kittyxiao,Lab2-1,1,100\n");
