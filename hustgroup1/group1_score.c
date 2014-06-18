d7bced58b69332ecda2a6778408d5b50f253d2b7
LoveCD <2416801271@qq.com>
2014-06-18 22:20:38 +0800

    cao

--- hustgroup1/group1_score.c
+++ hustgroup1/group1_score.c
@@ -6,7 +6,6 @@ main()
 //printf("This is for HUST ESD course 2014 score\n"); 
 printf("Xuhao, shmilyhh, 5, 90\n");
 printf("Xinyu, FCSCanal, 5, 90\n");
-<<<<<<< HEAD
 
 printf("houyuhan,hyieeh,helloworld,9,80\n");
 printf("houyuhan,hyieeh,coredump,8,80\n");
@@ -21,12 +20,11 @@ printf("houyuhan,hyieeh,lab4-3,4,100\n");
 printf("houyuhan,hyieeh,lab4-4,2,95\n");
 
 
-=======
 printf("Luzenong, KardLu, 5, 90\n");
 printf("Linmiaomiao, linmiaomiao, 5, 90\n");
 printf("houyuhan,hyieeh,5,90\n");
 printf("wangyuhao,wyhgithub,5,90\n");
->>>>>>> ec3fa03314ac89e3bc5cb0339a68592af48c9543
+
 return 0;
 
 }
