860af9a48fda6d4ec19081982ef4b2d247c91dca
QiaoYang <qiaoyang@ubuntu.ubuntu-domain>
2014-06-19 23:15:27 +0800

    qiaoyang

--- hustgroup5/group5_score.c
+++ hustgroup5/group5_score.c
@@ -2,6 +2,22 @@
 
 int main()
 {
+printf("group5,乔阳,yang1young,Lab1-1,1,100\n");
+printf("group5,乔阳,yang1young,Lab1-2,6,95\n");
+printf("group5,乔阳,yang1young,Lab2-1,2,90\n");
+printf("group5,乔阳,yang1young,Lab2-2,2,90\n");
+printf("group5,乔阳,yang1young,Lab2-3,2,90\n");
+printf("group5,乔阳,yang1young,Lab2-4,3,85\n");
+printf("group5,乔阳,yang1young,Lab2-5,2,90\n");
+printf("group5,乔阳,yang1young,Lab3-1,3,85\n");
+printf("group5,乔阳,yang1young,Lab3-2,1,100\n");
+printf("group5,乔阳,yang1young,Lab3-3,4,80\n");
+printf("group5,乔阳,yang1young,Lab3-4,3,85\n");
+printf("group5,乔阳,yang1young,Lab4-1,3,85\n");
+printf("group5,乔阳,yang1young,Lab4-2,5,79\n");
+printf("group5,乔阳,yang1young,Lab4-3,3,85\n");
+printf("group5,乔阳,yang1young,Lab4-4,5,79\n");
+
 printf("xiongfeng,xiongbanban,1,80\n");
 printf("xiongfeng,xiongbanban,2,80\n");
 printf("xiongfeng,xiongbanban,3,80\n");
