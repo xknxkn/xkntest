3efe85118955cbb0a1145acf72c0a5d2c224c687
巨荣辉 <jrhgreat@gmail.com>
2014-06-19 14:44:38 +0800

    巨荣辉提交，已经帮第6组更改

--- hustgroup3/group1_score.c
+++ hustgroup3/group1_score.c
@@ -4,11 +4,22 @@ main()
 {
 
 printf("This is for HUST ESD course 2014 score\n"); 
-printf("Xu Jiashen,peterxujia, 3,90\n");
-printf("Ouyangxi,Jimmy-ouyang, 3,90\n");
-printf("Juronghui,jrhgreat, 3,90\n");
-printf("Fengchiming,steveming, 3,90\n");
-printf("Huangxiaoyao,sigsawa, 3,90\n");
+printf("group3,巨荣辉,jrhgreat,Lab1-1,15,75\n");
+printf("group3,巨荣辉,jrhgreat,Lab1-2,15,75\n");
+printf("group3,巨荣辉,jrhgreat,Lab2-1,2,90\n");
+printf("group3,巨荣辉,jrhgreat,Lab2-2,1,100\n");
+printf("group3,巨荣辉,jrhgreat,Lab2-3,3,85\n");
+printf("group3,巨荣辉,jrhgreat,Lab2-4,3,85\n");
+printf("group3,巨荣辉,jrhgreat,Lab2-5,1,100\n");
+printf("group3,巨荣辉,jrhgreat,Lab3-1,2,90\n");
+printf("group3,巨荣辉,jrhgreat,Lab3-2,2,90\n");
+printf("group3,巨荣辉,jrhgreat,Lab3-3,3,85\n");
+printf("group3,巨荣辉,jrhgreat,Lab3-4,3,85\n");
+printf("group3,巨荣辉,jrhgreat,Lab3-5,2,90\n");
+printf("group3,巨荣辉,jrhgreat,Lab4-1,3,85\n");
+printf("group3,巨荣辉,jrhgreat,Lab4-2,3,85\n");
+printf("group3,巨荣辉,jrhgreat,Lab4-3,1,100\n");
+printf("group3,巨荣辉,jrhgreat,Lab4-4,2,90\n");
 
 
 return 0;
