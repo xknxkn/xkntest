3a259169ae59c0d5fb0753c4af84eba30f532da7
冯驰茗 <643513583@qq.com>
2014-06-19 15:00:03 +0800

    冯驰茗上交

--- hustgroup3/group1_score.c
+++ hustgroup3/group1_score.c
@@ -21,6 +21,24 @@ printf("group3,巨荣辉,jrhgreat,Lab4-2,3,85\n");
 printf("group3,巨荣辉,jrhgreat,Lab4-3,1,100\n");
 printf("group3,巨荣辉,jrhgreat,Lab4-4,2,90\n");
 
+printf("group3,冯驰茗,steveming,Lab1-1,20,70\n");
+printf("group3,冯驰茗,steveming,Lab1-2,20,70\n");
+printf("group3,冯驰茗,steveming,Lab2-1,2,90\n");
+printf("group3,冯驰茗,steveming,Lab2-2,1,100\n");
+printf("group3,冯驰茗,steveming,Lab2-3,3,85\n");
+printf("group3,冯驰茗,steveming,Lab2-4,3,85\n");
+printf("group3,冯驰茗,steveming,Lab2-5,1,100\n");
+printf("group3,冯驰茗,steveming,Lab3-1,2,90\n");
+printf("group3,冯驰茗,steveming,Lab3-2,2,90\n");
+printf("group3,冯驰茗,steveming,Lab3-3,3,85\n");
+printf("group3,冯驰茗,steveming,Lab3-4,3,85\n");
+printf("group3,冯驰茗,steveming,Lab3-5,2,90\n");
+printf("group3,冯驰茗,steveming,Lab4-1,3,85\n");
+printf("group3,冯驰茗,steveming,Lab4-2,3,85\n");
+printf("group3,冯驰茗,steveming,Lab4-3,1,100\n");
+printf("group3,冯驰茗,steveming,Lab4-4,2,90\n");
+
+
 
 return 0;
 }
