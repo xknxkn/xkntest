407ceab8b803aeb1e95a89917b02a4fee4971017
freewind2014 <2278873915@qq.com>
2014-06-20 09:16:20 +0800

    cao rong

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -3,11 +3,22 @@
 main()
 {
 printf("This is for HUST ESD course 2014 score\n"); 
-printf("buyao,byforbest, 1,90\n");
-printf("CaoRong,freewind2014, 1,90\n");
-printf("TanDongcai,tancongcai, 1,90\n");
-printf("SunLuyao,LuyaoSun, 1,90\n");
-printf("Yeyuanqing,yuanqingyeah, 1,90\n");
+printf("group1,曹荣,freewind, Lab1-1,1,90\n");
+printf("group1,曹荣,freewind, Lab1-2,1,90\n");
+printf("group1,曹荣,freewind, Lab2-1,1,90\n");
+printf("group1,曹荣,freewind, Lab2-2,1,90\n");
+printf("group1,曹荣,freewind, Lab2-3,1,90\n");
+printf("group1,曹荣,freewind, Lab2-4,1,90\n");
+printf("group1,曹荣,freewind, Lab2-5,1,90\n");
+printf("group1,曹荣,freewind, Lab3-1,1,90\n");
+printf("group1,曹荣,freewind, Lab3-2,1,90\n");
+printf("group1,曹荣,freewind, Lab3-3,1,90\n");
+printf("group1,曹荣,freewind, Lab3-4,1,90\n");
+printf("group1,曹荣,freewind, Lab3-5,1,90\n");
+printf("group1,曹荣,freewind, Lab4-1,1,90\n");
+printf("group1,曹荣,freewind, Lab4-2,1,90\n");
+printf("group1,曹荣,freewind, Lab4-3,1,90\n");
+printf("group1,曹荣,freewind, Lab4-4,1,90\n");
 return 0;
 
 }
