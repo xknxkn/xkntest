b4502d6683f9d9035c58d9578e36916399150ed7
freewind2014 <2278873915@qq.com>
2014-06-21 15:37:18 +0800

    曹荣  提交成绩

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -3,22 +3,100 @@
 main()
 {
 printf("This is for HUST ESD course 2014 score\n"); 
-printf("group1,曹荣,freewind, Lab1-1,1,90\n");
-printf("group1,曹荣,freewind, Lab1-2,1,90\n");
-printf("group1,曹荣,freewind, Lab2-1,1,90\n");
-printf("group1,曹荣,freewind, Lab2-2,1,90\n");
-printf("group1,曹荣,freewind, Lab2-3,1,90\n");
-printf("group1,曹荣,freewind, Lab2-4,1,90\n");
-printf("group1,曹荣,freewind, Lab2-5,1,90\n");
-printf("group1,曹荣,freewind, Lab3-1,1,90\n");
-printf("group1,曹荣,freewind, Lab3-2,1,90\n");
-printf("group1,曹荣,freewind, Lab3-3,1,90\n");
-printf("group1,曹荣,freewind, Lab3-4,1,90\n");
-printf("group1,曹荣,freewind, Lab3-5,1,90\n");
-printf("group1,曹荣,freewind, Lab4-1,1,90\n");
-printf("group1,曹荣,freewind, Lab4-2,1,90\n");
-printf("group1,曹荣,freewind, Lab4-3,1,90\n");
-printf("group1,曹荣,freewind, Lab4-4,1,90\n");
+printf("group2,曹荣,freewind, Lab1-1,17,70\n");
+printf("group2,曹荣,freewind, Lab1-2,11,75\n");
+printf("group2,曹荣,freewind, Lab2-1,1,100\n");
+printf("group2,曹荣,freewind, Lab2-2,2,90\n");
+printf("group2,曹荣,freewind, Lab2-3,3,85\n");
+printf("group2,曹荣,freewind, Lab2-4,3,85\n");
+printf("group2,曹荣,freewind, Lab3-1,1,100\n");
+printf("group2,曹荣,freewind, Lab3-2,3,85\n");
+printf("group2,曹荣,freewind, Lab3-3,2,90\n");
+printf("group2,曹荣,freewind, Lab3-4,4,80\n");
+printf("group2,曹荣,freewind, Lab4-1,6,70\n");
+printf("group2,曹荣,freewind, Lab4-2,6,70\n");
+printf("group2,曹荣,freewind, Lab4-3,6,70\n");
+printf("group2,曹荣,freewind, Lab4-4,6,70\n");
+
+
+printf("group2,歩遥,byforbest, Lab1-1,17,70\n");
+printf("group2,歩遥,byforbest, Lab1-2,11,75\n");
+printf("group2,歩遥,byforbest, Lab2-1,1,100\n");
+printf("group2,歩遥,byforbest, Lab2-2,2,90\n");
+printf("group2,歩遥,byforbest, Lab2-3,3,85\n");
+printf("group2,歩遥,byforbest, Lab2-4,3,85\n");
+printf("group2,歩遥,byforbest, Lab3-1,1,100\n");
+printf("group2,歩遥,byforbest, Lab3-2,3,85\n");
+printf("group2,歩遥,byforbest, Lab3-3,2,90\n");
+printf("group2,歩遥,byforbest, Lab3-4,4,80\n");
+printf("group2,歩遥,byforbest, Lab4-1,6,70\n");
+printf("group2,歩遥,byforbest, Lab4-2,6,70\n");
+printf("group2,歩遥,byforbest,Lab4-3,6,70\n");
+printf("group2,歩遥,byforbest, Lab4-4,6,70\n");
+
+
+
+printf("group2,孙路遥,LuyaoSun, Lab1-1,17,70\n");
+printf("group2,孙路遥,LuyaoSun, Lab1-2,11,75\n");
+printf("group2,孙路遥,LuyaoSun, Lab2-1,1,100\n");
+printf("group2,孙路遥,LuyaoSun, Lab2-2,2,90\n");
+printf("group2,孙路遥,LuyaoSun, Lab2-3,3,85\n");
+printf("group2,孙路遥,LuyaoSun, Lab2-4,3,85\n");
+printf("group2,孙路遥,LuyaoSun, Lab3-1,1,100\n");
+printf("group2,孙路遥,LuyaoSun, Lab3-2,3,85\n");
+printf("group2,孙路遥,LuyaoSun, Lab3-3,2,90\n");
+printf("group2,孙路遥,LuyaoSun, Lab3-4,4,80\n");
+printf("group2,孙路遥,LuyaoSun, Lab4-1,6,70\n");
+printf("group2,孙路遥,LuyaoSun, Lab4-2,6,70\n");
+printf("group2,孙路遥,LuyaoSun,Lab4-3,6,70\n");
+printf("group2,孙路遥,LuyaoSun, Lab4-4,6,70\n");
+
+printf("group2,叶源清,yuanqingyeah, Lab1-1,17,70\n");
+printf("group2,叶源清,yuanqingyeah, Lab1-2,11,75\n");
+printf("group2,叶源清,yuanqingyeah, Lab2-1,1,100\n");
+printf("group2,叶源清,yuanqingyeah, Lab2-2,2,90\n");
+printf("group2,叶源清,yuanqingyeah, Lab2-3,3,85\n");
+printf("group2,叶源清,yuanqingyeah, Lab2-4,3,85\n");
+printf("group2,叶源清,yuanqingyeah, Lab3-1,1,100\n");
+printf("group2,叶源清,yuanqingyeah, Lab3-2,3,85\n");
+printf("group2,叶源清,yuanqingyeah, Lab3-3,2,90\n");
+printf("group2,叶源清,yuanqingyeah, Lab3-4,4,80\n");
+printf("group2,叶源清,yuanqingyeah, Lab4-1,6,70\n");
+printf("group2,叶源清,yuanqingyeah, Lab4-2,6,70\n");
+printf("group2,叶源清,yuanqingyeah,Lab4-3,6,70\n");
+printf("group2,叶源清,yuanqingyeah, Lab4-4,6,70\n");
+
+printf("group2,谭栋材,LuyaoSun, Lab1-1,17,70\n");
+printf("group2,谭栋材,LuyaoSun, Lab1-2,11,75\n");
+printf("group2,谭栋材,LuyaoSun, Lab2-1,1,100\n");
+printf("group2,谭栋材,LuyaoSun, Lab2-2,2,90\n");
+printf("group2,谭栋材,LuyaoSun, Lab2-3,3,85\n");
+printf("group2,谭栋材,LuyaoSun, Lab2-4,3,85\n");
+printf("group2,谭栋材,LuyaoSun, Lab3-1,1,100\n");
+printf("group2,谭栋材,LuyaoSun, Lab3-2,3,85\n");
+printf("group2,谭栋材,LuyaoSun, Lab3-3,2,90\n");
+printf("group2,谭栋材,LuyaoSun, Lab3-4,4,80\n");
+printf("group2,谭栋材,LuyaoSun, Lab4-1,6,70\n");
+printf("group2,谭栋材,LuyaoSun, Lab4-2,6,70\n");
+printf("group2,谭栋材,LuyaoSun,Lab4-3,6,70\n");
+printf("group2,谭栋材,LuyaoSun, Lab4-4,6,70\n");
+
+printf("group2,叶凡,icyyyy, Lab1-1,17,70\n");
+printf("group2,叶凡,icyyyy, Lab1-2,11,75\n");
+printf("group2,叶凡,icyyyy, Lab2-1,1,100\n");
+printf("group2,叶凡,icyyyy, Lab2-2,2,90\n");
+printf("group2,叶凡,icyyyy, Lab2-3,3,85\n");
+printf("group2,叶凡,icyyyy, Lab2-4,3,85\n");
+printf("group2,叶凡,icyyyy, Lab3-1,1,100\n");
+printf("group2,叶凡,icyyyy, Lab3-2,3,85\n");
+printf("group2,叶凡,icyyyy, Lab3-3,2,90\n");
+printf("group2,叶凡,icyyyy, Lab3-4,4,80\n");
+printf("group2,叶凡,icyyyy, Lab4-1,6,70\n");
+printf("group2,叶凡,icyyyy, Lab4-2,6,70\n");
+printf("group2,叶凡,icyyyy,Lab4-3,6,70\n");
+printf("group2,叶凡,icyyyy, Lab4-4,6,70\n");
+
+
 return 0;
 
 }
