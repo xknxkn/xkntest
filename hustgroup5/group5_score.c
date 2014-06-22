7fb6a4db73cf4931a2ade36671d3e4d5c95fe7f3
熊峰 <1820342304@qq.com>
2014-06-22 11:01:28 +0800

    final test

--- hustgroup5/group5_score.c
+++ hustgroup5/group5_score.c
@@ -18,6 +18,7 @@ printf("group5,乔阳,yang1young,Lab4-2,5,79\n");
 printf("group5,乔阳,yang1young,Lab4-3,3,85\n");
 printf("group5,乔阳,yang1young,Lab4-4,5,79\n");
 
+printf("group5,乔阳,yang1young,Lab6-1,97\n");
 
 
 printf("group5,熊峰,xiongbanban,Lab1-1,22,70\n");
@@ -36,6 +37,8 @@ printf("group5,熊峰,xiongbanban,Lab4-2,5,79\n");
 printf("group5,熊峰,xiongbanban,Lab4-3,3,85\n");
 printf("group5,熊峰,xiongbanban,Lab4-4,5,79\n");
 
+printf("group5,熊峰,xiongbanban,Lab6-1,100\n");
+
 
 printf("group5,崔浩然,cuihaoran,Lab1-1,22,70\n");
 printf("group5,崔浩然,cuihaoran,Lab1-2,6,95\n");
@@ -53,6 +56,8 @@ printf("group5,崔浩然,cuihaoran,Lab4-2,5,79\n");
 printf("group5,崔浩然,cuihaoran,Lab4-3,3,85\n");
 printf("group5,崔浩然,cuihaoran,Lab4-4,5,79\n");
 
+printf("group5,崔浩然,cuihaoran,Lab6-1,89\n");
+
 printf("group5,陈晨,KKNWtinacc,Lab1-1,22,70\n");
 printf("group5,陈晨,KKNWtinacc,Lab1-2,6,95\n");
 printf("group5,陈晨,KKNWtinacc,Lab2-1,2,90\n");
@@ -68,6 +73,8 @@ printf("group5,陈晨,KKNWtinacc,Lab4-1,3,85\n");
 printf("group5,陈晨,KKNWtinacc,Lab4-2,5,79\n");
 printf("group5,陈晨,KKNWtinacc,Lab4-3,3,85\n");
 printf("group5,陈晨,KKNWtinacc,Lab4-4,5,79\n");
+
+printf("group5,陈晨,KKNWtinacc,Lab6-1,91\n");
 
 
 printf("group5,程天畅,CTCbaxia,Lab1-1,22,70\n");
@@ -85,6 +92,8 @@ printf("group5,程天畅,CTCbaxia,Lab4-1,3,85\n");
 printf("group5,程天畅,CTCbaxia,Lab4-2,5,79\n");
 printf("group5,程天畅,CTCbaxia,Lab4-3,3,85\n");
 printf("group5,程天畅,CTCbaxia,Lab4-4,5,79\n");
+
+printf("group5,程天畅,CTCbaxia,Lab6-1,89\n");
 
 return 0;
 }
