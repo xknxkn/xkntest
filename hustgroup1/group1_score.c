a7be05229a1c781b4ae14998ac178f9ceb90fe26
KardLu <michaelkard@gmail.com>
2014-06-22 08:44:29 +0800

    补充部分成绩

--- hustgroup1/group1_score.c
+++ hustgroup1/group1_score.c
@@ -19,12 +19,16 @@ printf("group1,侯雨涵,hyieeh,lab4-4,2,95\n");
 
 printf("group1,林苗苗,linmiaomiao,helloworld,5,85\n");
 printf("group1,林苗苗,linmiaomiao,coredump,8,80\n");
+printf("group1,林苗苗,linmiaomiao,lab2-1,1,100\n");
+printf("group1,林苗苗,linmiaomiao,lab2-2,1,100\n");
+printf("group1,林苗苗,linmiaomiao,lab2-3,1,100\n");
 printf("group1,林苗苗,linmiaomiao,lab2-4,1,100\n");
 printf("group1,林苗苗,linmiaomiaoh,lab3-1,1,100\n");
 printf("group1,林苗苗,linmiaomiao,lab3-2,3,90\n");
 printf("group1,林苗苗,linmiaomiao,lab3-3,1,100\n");
 printf("group1,林苗苗,linmiaomiao,lab3-4,1,100\n");
 printf("group1,林苗苗,linmiaomiao,lab3-5,1,100\n");
+printf("group1,林苗苗,linmiaomiao,lab4-1,4,80\n");
 printf("group1,林苗苗,linmiaomiao,lab4-2,4,80\n");
 printf("group1,林苗苗,linmiaomiao,lab4-3,4,100\n");
 printf("group1,林苗苗,linmiaomiao,lab4-4,2,95\n");
