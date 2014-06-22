40b351922016b0f926f6f815768c5148b0eb889c
xy <774224044@qq.com>
2014-06-22 11:17:04 +0800

    xy

--- hustgroup4/group1_score.c
+++ hustgroup4/group1_score.c
@@ -19,6 +19,8 @@ printf("group4,吕鹏原,lvpyuan,Lab4-1,1,100\n");
 printf("group4,吕鹏原,lvpyuan,Lab4-2,1,100\n");
 printf("group4,吕鹏原,lvpyuan,Lab4-3,2,98\n");
 printf("group4,吕鹏原,lvpyuan,Lab4-4,1,100\n");
+printf("group4,吕鹏原,lvpyuan,final,1,91\n");
+
 printf("group4,肖璇,kittyxiao,Lab1-1,10,80\n");
 printf("group4,肖璇,kittyxiao,Lab1-2,15,70\n");
 printf("group4,肖璇,kittyxiao,Lab2-1,1,100\n");
@@ -34,6 +36,7 @@ printf("group4,肖璇,kittyxiao,Lab4-1,1,100\n");
 printf("group4,肖璇,kittyxiao,Lab4-2,1,100\n");
 printf("group4,肖璇,kittyxiao,Lab4-3,2,98\n");
 printf("group4,肖璇,kittyxiao,Lab4-4,1,100\n");
+printf("group4,肖璇,kittyxiao,final,1,85\n");
 
 printf("group4,凌佳佳,lingjiajia,Lab1-1,12,75\n");
 printf("group4,凌佳佳,lingjiajia,Lab1-2,12,75\n");
@@ -50,6 +53,7 @@ printf("group4,凌佳佳,lingjiajia,Lab4-1,1,100\n");
 printf("group4,凌佳佳,lingjiajia,Lab4-2,1,100\n");
 printf("group4,凌佳佳,lingjiajia,Lab4-3,2,98\n");
 printf("group4,凌佳佳,lingjiajia,Lab4-4,1,100\n");
+printf("group4,凌佳佳,lingjiajia,final,1,77\n");
 
 printf("group4,薛彧,dowhat,Lab1-1,20,60\n");
 printf("group4,薛彧,dowhat,Lab1-2,19,60\n");
@@ -66,6 +70,7 @@ printf("group4,薛彧,dowhat,Lab4-1,1,100\n");
 printf("group4,薛彧,dowhat,Lab4-2,1,100\n");
 printf("group4,薛彧,dowhat,Lab4-3,2,98\n");
 printf("group4,薛彧,dowhat,Lab4-4,1,100\n");
+printf("group4,薛彧,dowhat,final,1,78\n");
 
 printf("group4,杜诗灏，LoveCD,Lab1-1,20,75\n");
 printf("group4,杜诗灏，LoveCD,Lab1-2,19,75\n");
@@ -82,6 +87,7 @@ printf("group4,杜诗灏，LoveCD,Lab4-1,1,100\n");
 printf("group4,杜诗灏，LoveCD,Lab4-2,1,100\n");
 printf("group4,杜诗灏，LoveCD,Lab4-3,2,95\n");
 printf("group4,杜诗灏，LoveCD,Lab4-4,1,100\n");
+printf("group4,杜诗灏，LoveCD,final,1,71\n");
 return 0;
 
 }
