4f0571c2bef590d3c0b228460fe45adce8706e53
xknxknqq <1553730177@qq.com>
2014-08-24 14:11:53 +0800

    add remove garbage

--- hust_esd_score.c
+++ hust_esd_score.c
@@ -3,8 +3,11 @@
 main()
 {
 
-printf("This is for HUST ESD course 2014 score\n"); 
-printf("Group,Name,GithubID,Chapter,Section,Score\n");
+printf("This is for SYSY ESD course 2014 score\n"); 
+
+printf("Group,StudentID,Name,GithubID,examination_score,Lab_score,Participation score,project score\n");
+
+printf("0,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
 
 return 0;
 }
--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -4,7 +4,7 @@ main()
 {
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
-printf("1,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
+printf("4,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
 
 return 0;
 }
