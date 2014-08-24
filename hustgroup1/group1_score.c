68d11325d718406f0d0bcec09a17d8957c8cceb3
Samantha <1216939898@qq.com>
2014-08-24 17:33:11 +0800

    madn

--- hustgroup1/group1_score.c
+++ hustgroup1/group1_score.c
@@ -7,5 +7,14 @@ main()
 ///printf("1,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
 printf("1,12353220,Yuechao xie,richardche,91,80,85,95\n");
 printf("1,12353253,Siyu Zeng,Cynthiarain,69,80,80,95\n");
+printf("1,12353199,Yuxuan Wang,cato1718,98,90,90,95\n");
+printf("1,12353048,Ruiqi Gong,Ricky-Gong,95,100,95,95\n");
+printf("1,12353064,Jiaxin Hong,HongJiaxin,92,85,85,95\n");
+printf("1,12353011,Jiajie Chen,CJJ100,94,85,95,95\n");
+printf("1,12353111,Zheng Li,hsqmlz,92,85,90,95\n");
+printf("1,12353079,Wenkai Huang,Instant7,91,90,100,95\n");
+printf("1,12353078,Shaoshu Huang,moonblamed,92,85,90,95\n");
+printf("1,14213824,Weihan Ren,PaulWeihan,93,100,90,95\n");
+
 return 0;
 }
