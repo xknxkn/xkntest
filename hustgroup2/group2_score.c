98ff3c5e7bd062b1077a1969043c6f8f5ccf3ae8
Jade Lynn <lynnjadecn@gmail.com>
2014-08-25 00:38:40 +0800

    score for group 2

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -4,7 +4,16 @@ main()
 {
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
-printf("2,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
+printf("2,12353121,Zhangwei Lin,lynnjadecn,89,95,75,100\n");
+printf("2,12353115,Yuhao Liang,LiangYuhao,90,80,75,100\n");
+printf("2,12353117,Kaichun Lin,lkcys,95,85,75,100\n");
+printf("2,12353142,Huaitian Lu,lutiantian,97,80,75,100\n");
+printf("2,12353246,Shuang Ye,shuangye114,95,95,75,100\n");
+printf("2,12353247,Xiqian Yi,Elvira-yxq,94,80,75,100\n");
+printf("2,12353167,Dan Qiao,FairyLord,75,80,75,100\n");
+printf("2,12353166,Xueqi Qian,HeyAmber,79,80,75,100\n");
+printf("2,12353156,Shiyue Nie,nnnsyyy,82,80,75,100\n");
+printf("2,12353115,Xiaojie Zhou,szxjzhou,91,100,95,100\n");
 
 return 0;
 }
