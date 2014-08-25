9c0d89c2fc9ef40074e106b3b7bf7a9eb05b92eb
HHBO <1084509768@qq.com>
2014-08-25 21:08:03 +0800

    group 6

--- hustgroup6/group6_score.c
+++ hustgroup6/group6_score.c
@@ -4,7 +4,14 @@ main()
 {
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
-printf("6,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
+printf("6,12353068,huangzhuolin,huangzhuolin,82,80,90,90\n");
+printf("6,12353074,huanghuanbo,HHBO,92,85,90,90\n");
+printf("6,12353065,hujunjie,everhujj,96,80,90,90\n");
+printf("6,12353295,lixueyan,544737612@qq.com,85,80,90,90\n");
+printf("6,12353277,Huadi Zheng,Hardyzheng,100,85,90,90\n");
+printf("6,12353195,Sihao Wang,2560934924@qq.com,75,85,80,90\n");
+printf("6,12353187,Jimin Tang,tangjm5@mail2.sysu.edu.cn,83,90,85,90\n");
+printf("6,12353082,Libin Jiang,jianglibin,76,85,85,90\n");
 
 return 0;
 }
