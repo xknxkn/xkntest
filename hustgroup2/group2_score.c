68dc0c72298c2f93f70cd01b8fdc3b35339e0517
yyyt1994 <yyyt1994@163.com>
2015-08-26 09:55:12 +0800

    yyyt1994

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -4,7 +4,7 @@ main()
 {
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
-printf("2,13354411,Yinyuting Yin,yyyt,100\n");
+printf("2,13354411,Yinyuting Yin,yyyt1994,100\n");
 
 return 0;
 }
