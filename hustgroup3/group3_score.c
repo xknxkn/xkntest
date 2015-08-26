7f6975a659cb49ea4a110c429e6afb647529d963
CecilQiu <qiuhongyu007@qq.com>
2015-08-26 17:07:56 +0800

    Qiu Hongyu commit the name of Qiu Hongyu, Lin Qifeng and Xie Bo

--- hustgroup3/group3_score.c
+++ hustgroup3/group3_score.c
@@ -7,6 +7,7 @@ int main()
    printf("3,13354173,李彦良,sysu-liyanliang,100,100,100,100\n"); 
    printf("3,13354286,邱宏宇,CecilQiu,100,100,100,100\n");
    printf("3,13354201,林奇峰,smielinqifeng,100,100,100,100\n");
+   printf("3,13354365,谢勃,xiebooooo123,100,100,100,100\n");
 
 return 0;
 }
