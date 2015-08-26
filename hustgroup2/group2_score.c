09bf5efa308a4c26caacd8e63341924aa8fb7f56
XuYuchen(13354376) <969977148@qq.com>
2015-08-26 02:32:57 -0700

    xuyuchen

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -11,5 +11,8 @@ printf ("2,13354419,zengliyi,helloworldzly,Section 1,100\n");
 printf("2,13354411,Yinyuting Yin,yyyt1994,100\n");
 
 printf("2, 13354225, liuyiping, lyptiffany, 100\n");
+
+
+printf("2, 13354376, xuyuchen, XuYuchen, 100\n");
 return 0;
 }
