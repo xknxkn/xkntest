101db434a7d4a07c69a9b2a964fa68f1ac1d7cfb
杜成乾 <dukeyunz@hotmail.com>
2015-08-26 23:17:04 +0800

    5 members have been pushed

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -3,17 +3,12 @@
 int main()
 {
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
-
 /*13 Grade Here*/
 
 printf("2,13354411,Yinyuting Yin,yyyt1994,100\n");
-
 printf("2, 13354225, liuyiping, lyptiffany, 100\n");
-
 printf("2, 13354376, xuyuchen, XuYuchen, 100\n");
-
 printf("2, 13354424, Zhaiqian, ZhaiQian, 100\n");
-
 printf("2, 12353039, duchengqian, sysu-yunz, 100\n");
 
 return 0;
