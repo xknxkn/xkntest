c576731b166b45ff6d1cf222dc153ed37b9b8698
logicchan <281944732@qq.com>
2015-08-26 11:58:06 +0800

    LOGICCHAN PUSHED SOMETHING

--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -5,7 +5,7 @@ main()
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
 //printf("4, 12330xxx, Kevin Xie, xknxknqq, 78, 90, 95, 100\n");
-printf("4,13354211,liuhang,Sysuliuhang, \n");
-printf("4,13354016,Chen Hong,logicchan,xx,xx,xx,xx\n");
+	printf("4,13354211,liuhang,Sysuliuhang, \n");
+	printf("4,13354016,Chen Hong,logicchan,xx,xx,xx,xx\n");
 	return 0;
 }
