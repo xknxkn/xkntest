816fb0353639c9e063986955da96677f263540e5
helloworldzly <182788062@qq.com>
2015-08-26 14:47:17 +0800

    zengliyii

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -5,7 +5,7 @@ int main()
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
 /*13 Grade Here*/
-printf ("zengliyi, helloworldzly,Section 1,100\n"); 
+printf ("2,13354419,zengliyi,helloworldzly,Section 1,100\n"); 
 
 return 0;
 }
