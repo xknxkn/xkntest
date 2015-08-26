86acbaea70361d54b87b3731f426f63adceb5078
helloworldzly <182788062@qq.com>
2015-08-26 23:42:29 +0800

    zengliyi++

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -4,8 +4,8 @@ int main()
 {
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 /*13 Grade Here*/
-
-printf("2,13354411,Yinyuting Yin,yyyt1994,100\n");
+printf("2, 13354419, zengliyi, helloworldzly, 100\n");
+printf("2, 13354411, Yinyuting Yin,yyyt1994, 100\n");
 printf("2, 13354225, liuyiping, lyptiffany, 100\n");
 printf("2, 13354376, xuyuchen, XuYuchen, 100\n");
 printf("2, 13354424, Zhaiqian, ZhaiQian, 100\n");
