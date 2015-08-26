8e7158dc2412ba2046738f38c74d91efbd3285a6
williamfzc <williamfzc@users.noreply.github.com>
2015-08-26 12:19:54 +0800

    Update group4_score.c

--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -7,5 +7,6 @@ main()
 //printf("4, 12330xxx, Kevin Xie, xknxknqq, 78, 90, 95, 100\n");
 	printf("4,13354211,liuhang,Sysuliuhang, \n");
 	printf("4,13354016,Chen Hong,logicchan,xx,xx,xx,xx\n");
+	printf("4,13354068,冯张弛,williamfzc,xx,xx,xx,xx\n");
 	return 0;
 }
