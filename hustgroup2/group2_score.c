cfa0daf45c9e1cf7f1bf57eded7e61b171af830c
helloworldzly <182788062@qq.com>
2015-08-26 14:10:09 +0800

    zengliyi

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -5,19 +5,7 @@ int main()
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
 /*13 Grade Here*/
-printf ("zengliyi, helloworldzly,Section 1,100\n"); //right??
-
-
-printf("2,12353121,Zhangwei Lin,lynnjadecn,89,95,75,100\n");
-printf("2,12353115,Yuhao Liang,LiangYuhao,90,80,75,100\n");
-printf("2,12353117,Kaichun Lin,lkcys,95,85,75,100\n");
-printf("2,12353142,Huaitian Lu,lutiantian,97,80,75,100\n");
-printf("2,12353246,Shuang Ye,shuangye114,95,95,75,100\n");
-printf("2,12353247,Xiqian Yi,Elvira-yxq,94,80,75,100\n");
-printf("2,12353167,Dan Qiao,FairyLord,75,80,75,100\n");
-printf("2,12353166,Xueqi Qian,HeyAmber,79,80,75,100\n");
-printf("2,12353156,Shiyue Nie,nnnsyyy,82,80,75,100\n");
-printf("2,12353115,Xiaojie Zhou,szxjzhou,91,100,95,100\n");
+printf ("zengliyi, helloworldzly,Section 1,100\n"); 
 
 return 0;
 }
