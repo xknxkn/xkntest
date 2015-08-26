378ec3538904e735d819639fc4bb0cedd41cd822
杜成乾 <dukeyunz@hotmail.com>
2015-08-26 23:14:01 +0800

    -.-||

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -16,5 +16,7 @@ printf("2, 13354225, liuyiping, lyptiffany, 100\n");
 printf("2, 13354376, xuyuchen, XuYuchen, 100\n");
 
 printf("2, 13354424, Zhaiqian, ZhaiQian, 100\n");
+
+printf("2, 12353039, duchengqian, sysu-yunz, 100\n");
 return 0;
 }
