ac5386ba3f1b95012eaa335f0a2ebeb67a6f896c
LiYanLiang <1151951667@qq.com>
2015-08-26 02:43:00 +0200

    liyanliang13354173 is coming!

--- hustgroup3/group3_score.c
+++ hustgroup3/group3_score.c
@@ -4,15 +4,7 @@ main()
 {
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
-   printf("3,12353033,陈月云,Yueyun,90,80,85,85\n"); 
-   printf("3,12353159,潘奕如,vivian94,92,80,90,85\n"); 
-   printf("3,12353030,陈奕芬,chenyifen,78,80,85,85\n"); 
-   printf("3,12353027,陈曦,luoji24,87,80,85,85\n"); 
-   printf("3,12353218,谢彤璇,Samantha,89,85,75,85\n");
-   printf("3,12353242,姚伟和,yaowh,84,80,100,85\n"); 
-   printf("3,12353229,许伟健,xuweijian,73,80,95,85\n"); 
-   printf("3,12330142,黄志伟,huangzhw6,81,80,85,85\n"); 
-   printf("3,12353241,姚灿武,NewHooker,89,80,85,85\n");
+   printf("3,13354173,liyanliang,liyanliang,100,100,100,100\n"); 
 
 return 0;
 }
