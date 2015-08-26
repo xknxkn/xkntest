9ce7ca476f875688cad60a71e26c75d26b4fa519
practicum <practicum@practicum-VirtualBox.(none)>
2015-08-26 04:59:07 +0200

    233333333333333333333

--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -5,15 +5,6 @@ main()
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
 //printf("4, 12330xxx, Kevin Xie, xknxknqq, 78, 90, 95, 100\n");
-printf("4,12353013,陈坚达,KinTaat,97,85,80,80\n");
-printf("4,12353003,蔡泽鑫,caizexin,86,85,85,80\n");
-printf("4,12353022,陈胜杰,Durant35,89,80,80,80\n");
-printf("4,12353296,钟奇立,872255922,95,80,80,80\n");
-printf("4,12353219,谢应严,IanTse,83,80,80,80\n");
-printf("4,12353204,温伟韬,simonwwt,84,80,80,80\n" );
-printf("4,12353254,曾晓霆,zengxiaoting,90,80,80,80\n");
-printf("4,12353004,查娟,zhajuan,96,85,80,80\n");
-printf("4,12353194,左莉佳,zuolijia,86,90,80,80\n");
-printf("4,12353020,陈秋良,zhiqiu,98,95,80,80\n");
+printf("4,13354211,liuhang,Sysuliuhang, \n");
 return 0;
 }
