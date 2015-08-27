579c9546fa77961936a9c5fbceef21e04b04267e
杜成乾 <dukeyunz@hotmail.com>
2015-08-27 14:27:33 +0800

    zhouxiaomei added

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -10,6 +10,7 @@ printf("2, 13354225, liuyiping, lyptiffany, 100\n");
 printf("2, 13354376, xuyuchen, XuYuchen, 100\n");
 printf("2, 13354424, Zhaiqian, ZhaiQian, 100\n");
 printf("2, 12353039, duchengqian, sysu-yunz, 100\n");
+printf("2, 13354481, zhouxiaomei, zhouxm6, 100\n");
 
 return 0;
 }
