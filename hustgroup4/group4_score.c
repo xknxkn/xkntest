9f454d08fff917c54df0854610665ada6ccd7508
yangxiaojun <1570406133@qq.com>
2015-08-27 17:03:36 +0800

    mhy

--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -12,5 +12,6 @@ int main()
 	printf("4,13354418,袁森淼,YuanSenmiao,100, 94, 90, 100\n");
 	printf("4,1335395,Yang_XiaoJun,0000000,95, 94, 90, 90\n");
 	printf("4,13354477,zhoujianfei,ZhouJianFei13354477,100,100,100,100 \n");
+printf("4,13354259,mahongyou,Mahy12,100,100,100,100 \n");
 	return 0;
 }
