ca4968ef3b64088825fc10518ad5be0de2330f56
xiwangzy <xiwangzy@foxmail.com>
2015-08-27 02:38:20 -0700

    allusers

--- hustgroup1/group1_score.c
+++ hustgroup1/group1_score.c
@@ -9,6 +9,11 @@ printf("13354460,Zhaoyue,xiwangzy,100\n");
 printf("13354212, Liujianbo, Bobbyliujb, 100\n");
 printf("13354291, Raochenyang, raochenyang, 100\n");
 printf("13354088, Haoshiwei, haoshw, 100\n");
-
+printf("13354156, Lijianli, Lijianli512, 100\n");
+printf("13354136, Kexiaohong, kexhong, 100\n");
+printf("13354314, Tanhaoxian, lovtam, 100\n");
+printf("13354325, Wangjianzheng, scp096 , 100\n");
+printf("13354093, Hezhongkang, hezhongkang , 100\n");
+printf("13354170, Lixinfeng, lixinfeng13, 100\n");
 return 0;
 }
