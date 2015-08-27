65c4f2b77a29c22a26cf7e507071f0a3bf026d8b
yangxiaojun <1570406133@qq.com>
2015-08-27 17:11:14 +0800

    yxj

--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -10,11 +10,11 @@ int main()
 	printf("4,13354068,冯张弛,williamfzc,xx,xx,xx,xx\n");
 	printf("4,13354237,卢宇沣,hallelujah237,hehe\n");
 	printf("4,13354418,袁森淼,YuanSenmiao,100, 94, 90, 100\n");
-	printf("4,1335395,Yang_XiaoJun,0000000,95, 94, 90, 90\n");
+	printf("4,1335395,Yang_XiaoJun,yangxj23,95, 94, 90, 90\n");
 	printf("4,13354477,zhoujianfei,ZhouJianFei13354477,100,100,100,100 \n");
-printf("4,13354259,mahongyou,Mahy12,100,100,100,100 \n");
-printf("4,13354247,luojintao,Luojt3,100,100,100,100 \n");
-printf("4,12353054,GuoLingfeng,GuoLingfeng,100,100,100,100 \n");
-printf("4,13354153,liguirong,LiGuiRong,100,100,100,100 \n");
+	printf("4,13354259,mahongyou,Mahy12,100,100,100,100 \n");
+	printf("4,13354247,luojintao,Luojt3,100,100,100,100 \n");
+	printf("4,12353054,GuoLingfeng,GuoLingfeng,100,100,100,100 \n");
+	printf("4,13354153,liguirong,LiGuiRong,100,100,100,100 \n");
 	return 0;
 }
