0c1a7327fb2e33f13b5d19796dd944e1f714062b
yangxj23 <1570406133@qq.com>
2015-08-28 17:41:16 +0800

    asd

--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -5,17 +5,17 @@ int main()
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
 //printf("4, 12330xxx, Kevin Xie, xknxknqq, 78, 90, 95, 100\n");
-	printf("4,13354211,liuhang,Sysuliuhang, 90\n");
-	printf("4,13354016,Chen Hong,logicchan,90\n");
+	printf("4,13354211,刘航,Sysuliuhang,70,100,100,90\n");
+	printf("4,13354016,陈鸿,logicchan,76,80,100,90\n");
 	printf("4,13354068,冯张弛,williamfzc,66,90,100,90 \n");
 	printf("4,13354237,卢宇沣,hallelujah237,90\n");
 	printf("4,13354418,袁森淼,YuanSenmiao,xx, 90, xx, xx\n");
-	printf("4,1335395,杨小军,yangxj23,95, 94, 90, 90\n");
-	printf("4,13354477,zhoujianfei,ZhouJianFei13354477,100,100,100,90\n");
-	printf("4,13354259,mahongyou,Mahy12,100,100,100,90\n");
-	printf("4,13354247,luojintao,Luojt3,100,100,100,90\n");
+	printf("4,1335395,杨小军,yangxj23,50, 90, 100, 90\n");
+	printf("4,13354477,周健飞,ZhouJianFei13354477,41,90,100,90\n");
+	printf("4,13354259,马宏有,Mahy12,74,100,100,90\n");
+	printf("4,13354247,罗锦涛,Luojt3,71,100,100,90\n");
 	printf("4,13354483,zhuhengbo,zhuhb,71,80,100,90\n");
-	printf("4,12353054,GuoLingfeng,GuoLingfeng,100,100,100,90\n");
+	printf("4,12353054,郭凌峰,GuoLingfeng,78,100,100,90\n");
 	printf("4,13354153,liguirong,LiGuiRong,68,85,100,90\n");
 	printf("4,13354176,lizhufeng,LiZhuFeng,65,50,100,90\n");
 	printf("4,13354133,揭英群,Vinsion1993,71,80,100,90\n");
@@ -23,5 +23,7 @@ int main()
 	printf("4,13354469,zhengxiaoxian,zhengxx,62,80,100,90\n");
 	printf("4,15213770,xiezengtian,thanks38,54,100,100,90\n");
 	printf("4,13354012,chenhang,aachenhang,84,100,100,90\n");
+	printf("4,12353232,杨安,Tryanddamere,58,50,50,90\n");
+
 	return 0;
 }
