ba2a21fd318b9cfdf5ef8ec50695b6e43fcbcf58
CecilQiu <qiuhongyu007@qq.com>
2015-08-28 19:46:19 +0800

    Qiu Hongyu commit the score.

--- hustgroup3/group3_score.c
+++ hustgroup3/group3_score.c
@@ -7,10 +7,6 @@ int main()
 
 
 
-   printf("3,13354173,쟀珞좁,sysu-liyanliang,100,100,100,100\n"); 
-   printf("3,13354286,헵브倒,CecilQiu,100,100,100,100\n");
-   printf("3,13354201,주펜룟,smielinqifeng,100,100,100,100\n");
-   printf("3,13354365,剋꺾,xiebooooo123,100,100,100,100\n");
    printf("3,13354118,黄愉,huangyu16,100,90,100,95\n");
    printf("3,13354408,叶绍轩,yeshaoxuan,100,90,100,95\n");
    printf("3,13354236,lu yian,kia95710,55,100,100,95\n");
@@ -19,6 +15,7 @@ int main()
    printf("3, 13354226, 刘宇威, DouMiaoO-Oo, 62, 70, 100, 95\n");
    printf("3, 13354358, 伍瑞萍, WuRuiping, 87, 80, 100, 95\n");
    printf("3,13354143, 蓝倩清，lqq12345,67,80,100,95\n");
+   printf("3,13354286,邱宏宇,CecilQiu,78,90,100,95\n");
 
 return 0;
 }
