2f21317474efc5cf02a30c96103bd9ff7e053922
liuyiping(13354225) <625130067@qq.com>
2015-08-28 07:24:33 -0700

    zhaiqian

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -8,7 +8,7 @@ printf("2, 13354419, zengliyi, helloworldzly, 89, 100, 100, 100\n");
 printf("2, 13354411, Yinyuting Yin,yyyt1994, 100\n");
 printf("2, 13354225, liuyiping, lyptiffany,89, 90, 100, 100\n");
 printf("2, 13354376, xuyuchen, XuYuchen, 100\n");
-printf("2, 13354424, Zhaiqian, ZhaiQian, 100\n");
+printf("2, 13354424, Zhaiqian, ZhaiQian,75, 90,100, 100\n");
 printf("2, 12353039, duchengqian, sysu-yunz, 100\n");
 printf("2, 13354481, zhouxiaomei, zhouxm6, 100\n");
 printf("2, 13354214, liujun, Liujun0325, 100\n");
