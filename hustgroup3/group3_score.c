7bc6be16b183e41cc7d3ab882645c8856d5433c4
WuRuiping <171129472@qq.com>
2015-08-28 17:49:28 +0800

    Update group3_score.c

--- hustgroup3/group3_score.c
+++ hustgroup3/group3_score.c
@@ -17,6 +17,7 @@ int main()
    printf("3,13354347,韦政源,weizhy3,80,100,100,95\n");
    printf("3, 13354374, 徐通, tt-leader, 66, 90, 100, 95\n");
    printf("3, 13354226, 刘宇威, DouMiaoO-Oo, 62, 70, 100, 95\n");
+   printf("3, 13354358, 伍瑞萍, WuRuiping, 87, 80, 100, 95\n");
 
 
 return 0;
