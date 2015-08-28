35db890a1f79f8b94acc7f31336dcdca5f1b1c35
Vinsion1993 <524702368@qq.com>
2015-08-28 02:17:28 -0700

    jieyq

--- hustgroup4/group4_score.c
+++ hustgroup4/group4_score.c
@@ -5,23 +5,21 @@ int main()
 /* groupid,studetnid,name,githubname,exam,lab,participation,project */
 
 //printf("4, 12330xxx, Kevin Xie, xknxknqq, 78, 90, 95, 100\n");
-	printf("4,13354211,liuhang,Sysuliuhang, \n");
-	printf("4,13354016,Chen Hong,logicchan,xx,xx,xx,xx\n");
-	printf("4,13354068,冯张弛,williamfzc,100,100,100,100 \n");
-	printf("4,13354237,卢宇沣,hallelujah237,hehe\n");
+	printf("4,13354211,liuhang,Sysuliuhang, 90\n");
+	printf("4,13354016,Chen Hong,logicchan,90\n");
+	printf("4,13354068,冯张弛,williamfzc,100,100,100,90 \n");
+	printf("4,13354237,卢宇沣,hallelujah237,90\n");
 	printf("4,13354418,袁森淼,YuanSenmiao,xx, 90, xx, xx\n");
 	printf("4,1335395,杨小军,yangxj23,95, 94, 90, 90\n");
-	printf("4,13354477,zhoujianfei,ZhouJianFei13354477,100,100,100,100 \n");
-	printf("4,13354259,mahongyou,Mahy12,100,100,100,100 \n");
-	printf("4,13354247,luojintao,Luojt3,100,100,100,100 \n");
-	printf("4,13354483,zhuhengbo,zhuhb,100,100,100,100 \n");
-
-
-	printf("4,12353054,GuoLingfeng,GuoLingfeng,100,100,100,100 \n");
-	printf("4,13354153,liguirong,LiGuiRong,100,100,100,100 \n");
-	printf("4,13354176,lizhufeng,LiZhuFeng,100,100,100,100 \n");
-	printf("4,13354133,揭英群,Vinsion1993,100,100,100,100 \n");
-	printf("4,13354239,路瑶,gongzifei,100,100,100,100 \n");
-	printf("4,13354469,zhengxiaoxian,zhengxx,100,100,100,100 \n");
+	printf("4,13354477,zhoujianfei,ZhouJianFei13354477,100,100,100,90\n");
+	printf("4,13354259,mahongyou,Mahy12,100,100,100,90\n");
+	printf("4,13354247,luojintao,Luojt3,100,100,100,90\n");
+	printf("4,13354483,zhuhengbo,zhuhb,71,80,100,90\n");
+	printf("4,12353054,GuoLingfeng,GuoLingfeng,100,100,100,90\n");
+	printf("4,13354153,liguirong,LiGuiRong,100,100,100,90\n");
+	printf("4,13354176,lizhufeng,LiZhuFeng,65,50,100,90\n");
+	printf("4,13354133,揭英群,Vinsion1993,71,80,71,90\n");
+	printf("4,13354239,路瑶,gongzifei,100,100,100,90\n");
+	printf("4,13354469,zhengxiaoxian,zhengxx,62,80,100,90\n");
 	return 0;
 }
