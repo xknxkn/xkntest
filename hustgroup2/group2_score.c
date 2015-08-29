0df558725679b0003bfcc38f017adf38a24a2eaf
Liujun0325 <317966543@qq.com>
2015-08-29 16:26:22 +0800

    Liu Jun commit the score

--- hustgroup2/group2_score.c
+++ hustgroup2/group2_score.c
@@ -13,7 +13,7 @@ printf("2, 13354376, xuyuchen, XuYuchen, 75, 90, 100, 100\n");
 printf("2, 13354424, Zhaiqian, ZhaiQian,75, 90,100, 100\n");
 printf("2, 12353039, duchengqian, sysu-yunz, 85，80，100，100\n");
 printf("2, 13354481, zhouxiaomei, zhouxm6, 70，80，100，100\n");
-printf("2, 13354214, liujun, Liujun0325, 100\n");
+printf("2, 13354214, liujun, Liujun0325, 66, 60, 100, 100\n");
 printf("2, 13354077, Gongjianfei, Jreffery, 82,90,100,100\n");
 printf("2, 13354130, Jiangxiaochen, Jiangxiaochen, 75,90,100,100\n");
 printf("2, 13354229, Liuzhaorong, Liuzhaor, 82,90,100,100\n");
