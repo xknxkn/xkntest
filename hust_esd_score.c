57cbcb54bcc9c1c0fa23ae386087a735528fb921
wzh9 <1506057560@qq.com>
2015-09-15 04:46:15 +0800

    add a line emply
    添加啦一行空行

--- hust_esd_score.c
+++ hust_esd_score.c
@@ -8,8 +8,9 @@ printf("This is for SYSY ESD course 2014 score\n");
 printf("Group,StudentID,Name,GithubID,examination_score,Lab_score,Participation score,project score\n");
 
 printf("0,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
-printf("0,1233o,wang zunhua,xknxknqq,80,80,80,80\n");
 
+
+printf("0,1233o,wang zunhua,xknxknqq,80,80,80,80\n");
 
 printf("0,111111111,zhu you hua,zyhzyh,90,90,90,90\n");
 
