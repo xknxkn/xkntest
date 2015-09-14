59231b303b10d5662525283771291be7783ee79d
wzh9 <1506057560@qq.com>
2015-09-15 04:40:16 +0800

    yuhkuoh

--- hust_esd_score.c
+++ hust_esd_score.c
@@ -8,6 +8,7 @@ printf("This is for SYSY ESD course 2014 score\n");
 printf("Group,StudentID,Name,GithubID,examination_score,Lab_score,Participation score,project score\n");
 
 printf("0,12330xxx,Kevin Xie,xknxknqq,78,90,95,100\n");
+printf("0,1233o,wang zunhua,xknxknqq,80,80,80,80\n");
 
 return 0;
 }
