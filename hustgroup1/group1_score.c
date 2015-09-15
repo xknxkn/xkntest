aefaeccc69ca993a764b6f5ae37881de170c3a92
crdiy <21915310@qq.com>
2015-09-15 11:32:47 +0800

    ge ran lian xi .....

--- hustgroup1/group1_score.c
+++ hustgroup1/group1_score.c
@@ -16,5 +16,6 @@ printf("1,13354325, Wangjianzheng, scp096 , 83,100,100,95\n");
 printf("1,13354093, Hezhongkang, hezhongkang , 88,80,100,95\n");
 printf("1,13354170, Lixinfeng, lixinfeng13, 82,90,100,95\n");
 printf("1,12353036, DaiKun, gonelikefly, 81,90,100,95\n");
+printf("Chen Ran,crdiy,1,50\n");
 return 0;
 }
